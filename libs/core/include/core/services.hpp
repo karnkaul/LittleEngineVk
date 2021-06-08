e22329663b2ca5f60dd2db8c948e7d2d5045f813
Karn Kaul <karnkaul@gmail.com>
2021-06-07 23:23:48 -0700

    Use array instead of umap for Services

--- libs/core/include/core/services.hpp
+++ libs/core/include/core/services.hpp
@@ -1,58 +1,68 @@
 #pragma once
-#include <unordered_map>
+#include <cstdint>
 #include <core/ensure.hpp>
 #include <core/not_null.hpp>
 #include <core/std_types.hpp>
 
 namespace le {
+template <std::size_t N>
+class ServiceLocator;
+
+using Services = ServiceLocator<16>;
+
 ///
 /// \brief Type-safe mapping for (pointers to) objects used as services
 ///
-class Services final {
+template <std::size_t N>
+class ServiceLocator final {
   public:
+	static constexpr std::size_t maxServices = N;
+
 	///
 	/// \brief Track passed service objects
 	///
 	template <typename... Ts>
 	static void track(not_null<Ts*>... ts) {
-		(s_ts.emplace(typeID<Ts>(), ts.get()), ...);
+		((s_ts[typeID<Ts>()] = ts.get()), ...);
 	}
 	///
 	/// \brief Untrack service objects
 	///
 	template <typename... Ts>
 	static void untrack() noexcept {
-		(s_ts.erase(typeID<Ts>()), ...);
+		((s_ts[typeID<Ts>()] = nullptr), ...);
 	}
 	///
 	/// \brief Clear tracked service objects
 	///
-	static void clear() noexcept { s_ts.clear(); }
+	static void clear() noexcept {
+		for (auto& p : s_ts) { p = nullptr; }
+	}
 	///
 	/// \brief Check if a service object corresponding to T exists
 	///
 	template <typename T>
-	static bool exists() noexcept {
-		return s_ts.contains(typeID<T>());
+	[[nodiscard]] static bool exists() noexcept {
+		return s_ts[typeID<T>()] != nullptr;
 	}
 	///
 	/// \brief Locate an existing service object
 	///
 	template <typename T>
-	static T* locate() noexcept {
-		if (auto it = s_ts.find(typeID<T>()); it != s_ts.end()) { return reinterpret_cast<T*>(it->second); }
-		ENSURE(false, "Service not found");
-		return nullptr;
+	[[nodiscard]] static T* locate() noexcept {
+		ENSURE(exists<T>(), "Service not found");
+		return reinterpret_cast<T*>(s_ts[typeID<T>()]);
 	}
 
   private:
 	template <typename T>
-	static u64 typeID() noexcept {
-		static u64 ret = ++s_typeID;
+	static std::size_t typeID() noexcept {
+		static std::size_t ret = ++s_typeID;
+		ENSURE(ret < maxServices, "Max services exceeded");
 		return ret;
 	}
 
-	inline static std::unordered_map<u64, void*> s_ts;
-	inline static u64 s_typeID = 0;
+	inline static void* s_ts[N] = {};
+	inline static std::size_t s_typeID = 0;
 };
 } // namespace le

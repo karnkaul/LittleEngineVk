6e733076ca8bdf9092db078966e85fccbe3758a1
Karn Kaul <karnkaul@gmail.com>
2021-09-04 15:24:31 -0700

    Update glfw/vulkan

--- libs/window/src/instance.cpp
+++ libs/window/src/instance.cpp
@@ -4,9 +4,6 @@
 #include <window/instance.hpp>
 
 #if defined(LEVK_USE_GLFW)
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
 #include <core/array_map.hpp>
 #include <core/ensure.hpp>
 #include <core/log.hpp>
